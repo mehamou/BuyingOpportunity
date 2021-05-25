f93a0357ffabc8469bfdd34fb3e56deefa0bd971
mehamou <mehdi.hamou+github@gmail.com>
2021-05-25 15:49:23 +0200

    first commit

---
+++ mainFBM.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+def document_initialised(driver):
+    return driver.execute_script("return initialised")
+
+DRIVER_PATH = '/Users/mehdihamou/Documents/ChromeDriver/chromedriver'
+
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get("https://www.autoscout24.fr/")
